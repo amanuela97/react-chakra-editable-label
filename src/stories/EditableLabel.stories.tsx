99b2e5804af7bc76fdae8b938ed638d1f2342a7d
amanuela97 <babyzewdie@gmail.com>
2023-07-15 22:09:44 +0300

    adding the library

---
+++ src/stories/EditableLabel.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react';
+import EditableLabel from '../components/EditableLabel';
+import { JSX } from 'react';
+import * as React from 'react';
+
+const meta: Meta<typeof EditableLabel> = {
+  component: EditableLabel,
+  title: 'Editable Label',
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof EditableLabel>;
+
+export const Example: Story = (
+  args: JSX.IntrinsicAttributes & {
+    text: string;
+    onSave: (value: string) => void;
+  }
+) => <EditableLabel {...args} />;
+
+Example.args = {
+  text: 'hello',
+  onSave: (value: string) => console.log(value),
+};
