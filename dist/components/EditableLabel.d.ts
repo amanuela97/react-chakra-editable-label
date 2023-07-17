27798733b677e4df18ce0f8fb619cf1bcebd9f6c
amanuela97 <babyzewdie@gmail.com>
2023-07-17 15:43:25 +0300

    fix: remove component folder

--- dist/components/EditableLabel.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-type Props = {
-    text: string;
-    onSave: (value: string) => void;
-};
-declare const EditableLabel: ({ text, onSave }: Props) => React.JSX.Element;
-export default EditableLabel;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,7 @@
-export * from './components/EditableLabel';
+import * as React from 'react';
+type Props = {
+  text: string;
+  onSave: (value: string) => void;
+};
+declare const EditableLabel: ({ text, onSave }: Props) => React.JSX.Element;
+export default EditableLabel;
